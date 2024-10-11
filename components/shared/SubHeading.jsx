65eb0b09411909228a09eadf1b71a1e43134e965
Nilesh Panda <nileshpanda@Nileshs-MacBook-Pro.local>
2024-10-11 14:33:40 +0530

    Minor Change v2

--- components/shared/SubHeading.jsx
+++ components/shared/SubHeading.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function SubHeading() {
-  return (
-    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-primaryText">
-      Subheading
-    </h2>
-  )
+  return <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-primaryText">Subheading Title</h2>;
 }
 
-export default SubHeading
+export default SubHeading;
