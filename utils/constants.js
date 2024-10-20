0ff1d06314f7950085e8ca6ea2f7182267db625c
Nilesh Panda <nileshpanda@Nileshs-MacBook-Pro.local>
2024-10-20 12:15:20 +0530

    pleasureable video added v3

--- utils/constants.js
+++ utils/constants.js
@@ -164,8 +164,8 @@ export const welcomePageParagraphsVideos = [
     content: `At JNG Homes, we don’t just build houses; we craft dreams with unwavering integrity and a commitment to excellence. Our vision is clear and uncompromising, grounded in honesty that flows through every conversation, pricing decision, and material choice we make.
       Our team isn’t just here to meet standards; we set them. With a passion for exceptional service and an unparalleled level of care, we treat each project as a unique journey, ensuring that your vision comes to life with the utmost precision. At JNG Homes, we believe that a home is more than just a structure—it’s a reflection of who you are, built on a foundation of trust and dedication. Join us in creating spaces that inspire and elevate the everyday.
       `,
-    videoURL: "",
-    videoPoster: "https://res.cloudinary.com/dchzeybwq/video/upload/v1729278049/video_1_home_page_qdmpdn.mp4",
+    videoURL: "https://res.cloudinary.com/dchzeybwq/video/upload/v1729278049/video_1_home_page_qdmpdn.mp4",
+    videoPoster: "",
   },
   {
     sl: 3,
