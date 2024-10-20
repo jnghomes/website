524a55aa24f338eea42b10f764f2c2b68e8d3cb3
Nilesh Panda <nileshpanda@Nileshs-MacBook-Pro.local>
2024-10-20 12:33:28 +0530

    added email changes

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,34 +1,5 @@
 import ContactUsPage from "@/components/pages/ContactUsPage";
 import React from "react";
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "info@jnghomes.in",
-    pass: process.env.GMAIL_PASSWORD,
-  },
-});
-
-console.log(process.env.GMAIL_PASSWORD);
-
-const mailOptions = {
-  from: "info@jnghomes.in",
-  to: "info@jnghomes.in",
-  subject: "New Enquiry",
-  text: "This is a test email sent using Nodemailer.",
-};
-
-// transporter.sendMail(mailOptions, (error, info) => {
-//   if (error) {
-//     console.error("Error sending email: ", error);
-//   } else {
-//     console.log("Email sent: ", info.response);
-//   }
-// });
 
 function page() {
   return (
--- components/pages/ContactUsPage.jsx
+++ components/pages/ContactUsPage.jsx
@@ -3,11 +3,26 @@ import { welcomePageProjects } from "@/utils/constants";
 import React, { useState } from "react";
 import Heading from "../shared/Heading";
 
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "info@jnghomes.in",
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+console.log(JSON.stringify(process.env));
+
 function ContactUsPage() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     project: "",
+    phoneNumber: "",
   });
   const [error, setError] = useState("");
 
@@ -21,9 +36,9 @@ function ContactUsPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, email, project } = formData;
+    const { name, email, project, phoneNumber } = formData;
 
-    if (!name || !email || !project) {
+    if (!name || !email || !project || !phoneNumber) {
       setError("All fields are required.");
       return;
     }
@@ -32,8 +47,20 @@ function ContactUsPage() {
       setError("Please enter a valid email address.");
       return;
     }
+    const mailOptions = {
+      from: "info@jnghomes.in",
+      to: "info@jnghomes.in",
+      subject: `New Enquiry for ${project}`,
+      text: `New Enquiry for ${project} by ${name}. Email: ${email}, Phone Number: ${phoneNumber}`,
+    };
 
-    // Submit the form (e.g., send email with nodemailer)
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+      } else {
+        console.log("Email sent: ", info.response);
+      }
+    });
     console.log("Form submitted with data:", formData);
     setFormData({
       name: "",
@@ -84,6 +111,24 @@ function ContactUsPage() {
         </label>
 
         <label
+          htmlFor="Email"
+          className="relative block rounded-md border border-primaryText shadow-sm focus-within:border-primaryAccent focus-within:ring-2 focus-within:ring-primaryAccent"
+        >
+          <input
+            type="text"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            className="w-full peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 p-2"
+            placeholder="Email"
+          />
+          <span className="bg-background pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 p-0.5 text-xs text-primaryText transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+            Phone Number
+          </span>
+        </label>
+
+        <label
           htmlFor="Project"
           className="relative block rounded-md border border-primaryText shadow-sm focus-within:border-primaryAccent focus-within:ring-2 focus-within:ring-primaryAccent"
         >
@@ -102,9 +147,7 @@ function ContactUsPage() {
                 key={project.sl}
                 value={project.url}
                 disabled={project.bookingStatus !== "Open"}
-                className={
-                  project.bookingStatus !== "Open" ? "text-gray-400" : "text-primaryText"
-                }
+                className={project.bookingStatus !== "Open" ? "text-gray-400" : "text-primaryText"}
               >
                 {project.title} - {project.bookingStatus}
               </option>
@@ -114,10 +157,7 @@ function ContactUsPage() {
 
         {error && <p className="text-red-500 text-sm">{error}</p>}
 
-        <button
-          type="submit"
-          className="bg-primaryAccent text-white px-4 py-2 rounded-md"
-        >
+        <button type="submit" className="bg-primaryAccent text-white px-4 py-2 rounded-md">
           Submit
         </button>
       </form>
